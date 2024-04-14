c929731eb4290e3f3a44bbcd7230bfa289a72244
ricky <125274483+patrickNwafo@users.noreply.github.com>
2024-04-14 16:44:57 +0100

    seventh project

--- 5.5 REST APIs/solution.js
+++ 5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "7a850eed-05fc-4d3f-b567-e3774d41662c";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
